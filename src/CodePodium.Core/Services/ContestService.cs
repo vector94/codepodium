d9647b5a40b4120ca218a30a0e2801b773b9263d
Md Asif Iqbal Ahmed <asif.ahmed9414@gmail.com>
2026-03-03 18:05:29 +0100

    add ContestService

---
+++ src/CodePodium.Core/Services/ContestService.cs
@@ -0,0 +1,33 @@
+using CodePodium.Core.Interfaces;
+using CodePodium.Core.Models;
+
+namespace CodePodium.Core.Services;
+
+public class ContestService(IContestRepository contestRepository, IEnumerable<IContestFetcher> fetchers)
+{
+    public async Task SyncContestsAsync()
+    {
+        foreach (var fetcher in fetchers)
+        {
+            var contests = await fetcher.FetchContestsAsync();
+            foreach (var contest in contests)
+                await contestRepository.AddAsync(contest);
+        }
+        await contestRepository.SaveChangesAsync();
+    }
+
+    public Task<(IEnumerable<Contest> Items, int TotalCount)> GetPagedContestsAsync(string? platform, int page, int pageSize) =>
+        contestRepository.GetPagedAsync(platform, page, pageSize);
+
+    public Task<IEnumerable<Contest>> GetAllContestsAsync() =>
+        contestRepository.GetAllAsync();
+
+    public Task<IEnumerable<Contest>> GetUpcomingContestsAsync() =>
+        contestRepository.GetUpcomingAsync();
+
+    public Task<IEnumerable<Contest>> GetContestsByPlatformAsync(string platform) =>
+        contestRepository.GetByPlatformAsync(platform);
+
+    public Task<Contest?> GetContestByIdAsync(int id) =>
+        contestRepository.GetByIdAsync(id);
+}
